c5006c828e783d65adefee72cc7492f1ee70c73d
Rushikesh <rushigaware1020@gmail.com>
2023-10-12 00:01:26 +0530

    check the function if is it correct

---
+++ primeProblem.js
@@ -0,0 +1,16 @@
+function checkPrime(N) {
+          let count = 0
+          for (let i = 1; i <= N; i++) {
+                    if (N % i == 0) {
+                              count += 1
+                    }
+          }
+
+          if (count == 1) {
+                    console.log(N, "is Prime")
+          }
+          else {
+                    console.log(N, "is not Prime")
+          }
+}
+checkPrime(13)
